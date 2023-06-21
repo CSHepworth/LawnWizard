014c3a166d4355a2ccbb96146f466fb6064e5db0
CSHepworth <109162129+CSHepworth@users.noreply.github.com>
2023-06-20 22:40:19 -0600

    Created Platform

--- lawn-wizard-platform/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace lawn_wizard_platform.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ lawn-wizard-platform/Models/LawnWizardContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace lawn_wizard_platform.Models
+{
+    public class LawnWizardContext : DbContext
+    {
+        public LawnWizardContext(DbContextOptions<LawnWizardContext> options)
+            : base(options) 
+        {
+        }
+    }
+}
